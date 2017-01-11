3e25ea03d44aa5100ee864869000048108cca084
laure <laure@laure-VirtualBox>
2017-01-11 09:26:58 +0100

    changement sur MonButton

--- src/com/imie/javaSwingExe/MonButton.java
+++ src/com/imie/javaSwingExe/MonButton.java
@@ -16,16 +16,9 @@ public class MonButton extends JButton implements MouseListener {
 		
 	}
 	public void paintComponent(Graphics g){
-	    Graphics2D g2d = (Graphics2D)g;
-	    GradientPaint gp = new GradientPaint(0, 0, Color.blue, 0, 20, Color.cyan, true);
-	    g2d.setPaint(gp);
-	    g2d.fillRect(0, 0, this.getWidth(), this.getHeight());
-	    g2d.setColor(Color.white);
-	    g2d.drawString(this.name, this.getWidth() / 2 - (this.getWidth()/ 2 /4), (this.getHeight() / 2) + 5);
-	  //* le carre  
-	    
+	       
 	    g.setColor(Color.BLUE);
-	    g.fillRect(); 
+	    g.fillRect()
 	
 	}     
 
